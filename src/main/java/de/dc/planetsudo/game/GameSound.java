06bc2023b6350a58ae969640c62ed21e82aaf49b
Divine Threepwood <divine@openbase.org>
2013-10-08 20:42:58 +0000

    Sounds fixed

--- src/main/java/de/dc/planetsudo/game/GameSound.java
+++ src/main/java/de/dc/planetsudo/game/GameSound.java
@@ -4,6 +4,7 @@
  */
 package de.dc.planetsudo.game;
 
+import de.dc.util.logging.Logger;
 import de.dc.util.sound.AudioServer;
 import de.dc.util.sound.FileHolder;
 import java.io.File;
@@ -24,28 +25,38 @@ public enum GameSound implements FileHolder {
 	 * - (tanken)
 	 * - Unterstützen
 	 * Resource abliefern	 */
-	DeployMine("sound/sound.wav"),
-	DeployMarker("sound/sound.wav"),
-	RechargeFuel("sound/sound.wav"),
-	CallForSupport("sound/sound.wav"),
-	SpendFuel("sound/sound.wav"),
-	AgentExplosion("sound/sound.wav"),
-	MothershipExplosion("sound/sound.wav"),
-	DeliverResource("sound/sound.wav"),
-	Laser("sound/sound.wav");
-
-	private final File soundFile;
-
+	DeployMine("sound/deplay_mine.wav"),
+	DeployMarker("sound/set_marker.wav"),
+	RechargeFuel("sound/recharge_fuel.wav"),
+	CallForSupport("sound/lasershot.wav"),
+	SpendFuel("sound/spend_fuel.wav"),
+	AgentExplosion("sound/explosion_agent.wav"),
+	MothershipExplosion("sound/explosion_mothership.wav"),
+	MothershipUnderAttack("sound/attack_mothership.wav"),
+	DeliverResource("sound/deliver_resource.wav"),
+	Laser("sound/lasershot.wav"),
+	AgentDisabled("sound/agent_empty_fuel.wav");
+	private File soundFile;
+	private boolean disabled;
+	@Override
 	public File getFile() {
 		return soundFile;
 	}
 
-	private GameSound(String uri) {
-		this.soundFile = new File(ClassLoader.getSystemResource(uri).getFile());
+	private GameSound(final String uri) {
+		try {
+			this.soundFile = new File(ClassLoader.getSystemResource(uri).getFile());
+			disabled = false;
+		} catch (Exception ex) {
+			Logger.warn(this, "Could not load Soundfile["+uri+"] of "+name() ,ex);
+			disabled = true;
+		}
 	}
 
 	public void play() {
-		AudioServer.playClip(SpendFuel);
+		if(disabled) {
+			return;
+		}
+		AudioServer.playClip(this);
 	}
 }
-
