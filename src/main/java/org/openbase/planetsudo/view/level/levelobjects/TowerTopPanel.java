f6e6322e8dbd5d08984e715f4866caecf007e2fb
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-01-10 20:57:48 +0100

    License header 2017 upgrade.

--- src/main/java/org/openbase/planetsudo/view/level/levelobjects/TowerTopPanel.java
+++ src/main/java/org/openbase/planetsudo/view/level/levelobjects/TowerTopPanel.java
@@ -4,7 +4,7 @@ package org.openbase.planetsudo.view.level.levelobjects;
  * #%L
  * PlanetSudo GameEngine
  * %%
- * Copyright (C) 2009 - 2016 openbase.org
+ * Copyright (C) 2009 - 2017 openbase.org
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
