cce0ac29e97fc4965d772fcb5805cb8433f5e61c
Divine Threepwood <divine@openbase.org>
2024-01-23 23:56:42 +0100

    add github action pipeline

---
+++ prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+WHITE='\033[0;37m'
+
+APP_NAME='planetsudo engine'
+APP_NAME=${BLUE}${APP_NAME}${NC}
+echo -e "=== ${APP_NAME} project ${WHITE}cleanup${NC}"
+./gradlew clean --quiet $@
+echo -e "=== ${APP_NAME} project ${WHITE}installation${NC}"
+echo -e "nothing to prepare..."
+echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed."
